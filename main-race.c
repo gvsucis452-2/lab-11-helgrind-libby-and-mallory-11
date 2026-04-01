21a1a11cf1b7d06ef38c8a323ae51206a09c3813
LibGres <gresleyl@mail.gvsu.edu>
2026-04-01 11:37:28 -0400

    GRADE ME

--- main-race.c
+++ main-race.c
@@ -1,18 +1,22 @@
 #include <stdio.h>
-
 #include "common_threads.h"
 
 int balance = 0;
+pthread_mutex_t m1 = PTHREAD_MUTEX_INITIALIZER;
 
 void* worker(void* arg) {
-    balance++; // unprotected access 
+    Pthread_mutex_lock(&m1);
+    balance++; // unprotected access
+    Pthread_mutex_unlock(&m1);
     return NULL;
 }
 
 int main(int argc, char *argv[]) {
     pthread_t p;
     Pthread_create(&p, NULL, worker, NULL);
+    Pthread_mutex_lock(&m1);
     balance++; // unprotected access
+    Pthread_mutex_unlock(&m1);
     Pthread_join(p, NULL);
     return 0;
 }
